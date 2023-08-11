bbb3135e909fa4fda8982f5456f2473a0949f692
Bibi Izna <izna8402@gmail.com>
2023-08-11 22:43:02 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,7 +1,6 @@
 
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-        # Add the start and end points of the stick to the cuts list
         cuts.append(0)
         cuts.append(n)
         cuts.sort()
