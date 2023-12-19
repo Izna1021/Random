65b04fb3373f15d8a78e75634daa1e6c2b89b8c5
Bibi Izna <izna8402@gmail.com>
2023-12-19 19:31:51 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -3,9 +3,7 @@ class MyHashSet:
         self.size = 1000
         self.buckets = [[] for _ in range(self.size)]
     def _hash(self, key):
-
         return key % self.size
-
     def add(self, key):
 
         if not self.contains(key):
